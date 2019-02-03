de0597de8ea652c00962c78a04ceb068ec826e64
Daniel Martin <danielt8642@gmail.com>
2019-02-02 18:31:55 -0800

    Built main page skeleton

--- src/App.js
+++ src/App.js
@@ -46,13 +46,10 @@ class App extends Component {
             <Collapse isOpen={this.state.isOpen} navbar>
               <Nav className="nav-items ml-auto" navbar>
                 <NavItem>
-                  <NavLink href={"#places"}>Blog</NavLink>
+                  <NavLink href={"#blog"}>Blog</NavLink>
                 </NavItem>
                 <NavItem>
                   <NavLink href={"#videos"}>Videos</NavLink>
-                </NavItem>
-                <NavItem>
-                  <NavLink href={"#photography"}>Photos</NavLink>
                 </NavItem>
                 <NavItem>
                   <NavLink href={"#about"}>About</NavLink>
@@ -71,7 +68,7 @@ class App extends Component {
               <Col>
                 <p>Love For The Outdoors</p>
                 <p>
-                  <Button tag="a" color="success" size="large" href={"#places"}>
+                  <Button tag="a" color="success" size="large" href={"#blog"}>
                     Explore
                   </Button>
                 </p>
@@ -79,29 +76,145 @@ class App extends Component {
             </Row>
           </Container>
         </div>
-        <div className="places" id="places">
+        <div className="blog" id="blog">
+          <Container>
+            <h1>Blog</h1>
+            <Row className="d-flex">
+              <Col>
+                <a href="#">
+                  <img
+                    className="rounded-circle"
+                    src={require("./img/zion-canyon.jpg")}
+                    alt=""
+                  />
+                </a>
+                <a href="#">
+                  <h3>Zion National Park</h3>
+                  <p>The most magical place in the American desert!</p>
+                </a>
+              </Col>
+              <Col>
+                <a href="#">
+                  <img
+                    className="rounded-circle"
+                    src={require("./img/crested-butte-fall.jpg")}
+                    alt=""
+                  />
+                </a>
+                <a href="#">
+                  <h3>Rocky Mountains</h3>
+                  <p>
+                    Pheonomenal fall colors in Western Colorado do not
+                    disappoint.
+                  </p>
+                </a>
+              </Col>
+              <Col>
+                <a href="#">
+                  <img
+                    className="rounded-circle"
+                    src={require("./img/san-diego-coast.jpg")}
+                    alt=""
+                  />
+                </a>
+                <a href="#">
+                  <h3>California Coast</h3>
+                  <p>
+                    Anywhere along the Pacific Coast Highway is a place to stop
+                    and take in the view!
+                  </p>
+                </a>
+              </Col>
+            </Row>
+          </Container>
+        </div>
+        <div className="videos" id="videos">
+          <h1>Videos</h1>
+          <Container>
+            <Row className="row">
+              <Col>
+                <iframe
+                  src="https://www.youtube.com/embed/vbG6sN6dfCk"
+                  frameborder="0"
+                  allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+                  allowfullscreen
+                />
+              </Col>
+              <Col>
+                <h3>Great Smoky Mountains</h3>
+                <h5>Alum Cave Trail</h5>
+                <p>
+                  Join us as we hike to Alum Cave on the Tennessee side of Great
+                  Smoky Mountains National Park! Alum Cave can be accessed via
+                  the trail to the summit of Mt Le Conte and is about half way
+                  up, offering beautiful views of the surrounding mountains
+                  amidst the bluffs!
+                </p>
+              </Col>
+            </Row>
+            <Row className="row">
+              <Col>
+                <iframe
+                  src="https://www.youtube.com/embed/nHWv6SiL1Gs"
+                  frameborder="0"
+                  allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+                  allowfullscreen
+                />
+              </Col>
+              <Col>
+                <h3>Zion National Park</h3>
+                <h5>Angel's Landing Trail</h5>
+                <p>
+                  We truly believe Angel's Landing is the best trail in Zion
+                  National Park, hands down! This trail offers one of the most
+                  unique hiking experiences you can ever have.
+                </p>
+              </Col>
+            </Row>
+            <Row className="row">
+              <Col>
+                <iframe
+                  src="https://www.youtube.com/embed/E_oJAkvobP0"
+                  frameborder="0"
+                  allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+                  allowfullscreen
+                />
+              </Col>
+              <Col>
+                <h3>Grand Teton National Park</h3>
+                <h5>Phelps Lake Trail</h5>
+                <p>
+                  We truly believe Angel's Landing is the best trail in Zion
+                  National Park, hands down! This trail offers one of the most
+                  unique hiking experiences you can ever have.
+                </p>
+              </Col>
+            </Row>
+          </Container>
+        </div>
+        <div className="about" id="about">
           <Container>
             <Row>
               <Col>
-                <img
-                  className="rounded-circle"
-                  src={require("./img/zion-canyon.jpg")}
-                />
-                <h3>Zion</h3>
+                <h1>About</h1>
+                <p>
+                  Now this is a story all about how my life got twist-turned
+                  upside-down!
+                </p>
               </Col>
+            </Row>
+          </Container>
+        </div>
+        <div className="contact" id="contact">
+          <Container>
+            <Row>
               <Col>
-                <img
-                  className="rounded-circle"
-                  src={require("./img/crested-butte-fall.jpg")}
-                />
-                <h3>Rocky Mountains</h3>
-              </Col>
-              <Col>
-                <img
-                  className="rounded-circle"
-                  src={require("./img/san-diego-coast.jpg")}
-                />
-                <h3>San Diego</h3>
+                <h1>Contact</h1>
+                <hr id="email-split" />
+                <p>
+                  <a>email@gmail.com</a>
+                </p>
+                <hr id="email-split" />
               </Col>
             </Row>
           </Container>
